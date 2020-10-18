41ce7ed6a9d7a4f8a8238d5f53595d6ff3de23ac
Andrey <uistinkatu5a17@yandex.ru>
2020-10-19 01:05:32 +0300

    commit #2. Rebuilding of the file structure

--- app/App/Http/Controller.php
+++ app/App/Http/Controller.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * Class Controller
+ * @package App\Http
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
